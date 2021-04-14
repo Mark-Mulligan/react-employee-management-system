929add40c8556c53f56bbf57489fb6088a736bc8
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-14 14:10:01 -0500

    creating pages

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Employee Management System</h1>
+    </div>
+  )
+}
+
+export default LandingPage;
