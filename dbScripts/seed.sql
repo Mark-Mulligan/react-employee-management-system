fa729e9f03f05cc7b4c662020af05ef917955d3a
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-14 21:12:31 -0500

    added sql seed script

---
+++ dbScripts/seed.sql
@@ -0,0 +1,46 @@
+Drop Database IF exists employee_management_db;
+CREATE DATABASE employee_management_db;
+
+Use employee_management_db;
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(40),
+    password VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY Key,
+    name VARCHAR(30),
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY Key,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY Key,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    date_hired DATE,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
