d53e33e03c26cfc2892b572409ec823994de7a85
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-15 22:37:12 -0500

    added more pages

---
+++ client/src/pages/AllEmployeesPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "first_name", headerName: "First" },
+  { field: "last_name", headerName: "Last" },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 150,
+  },
+  {
+    field: "department",
+    headerName: "Department",
+    width: 150,
+  },
+  {
+    field: "salary",
+    headerName: "Salary",
+    type: "number",
+    valueFormatter: ({ value }) => `$${value.toLocaleString()}`,
+  },
+  {
+    field: "manager",
+    headerName: "Manager",
+    width: 150,
+    valueFormatter: ({ value }) => (!value ? "N/A" : value),
+  },
+];
+
+const AllEmployeesPage = () => {
+  return (
+    <div>All Employees Page</div>
+  )
+}
+
+export default AllEmployeesPage;
