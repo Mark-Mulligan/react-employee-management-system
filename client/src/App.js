6a372e2bdae46220855ae02c44bed7ccd273c26c
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-15 19:39:45 -0500

    front end routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
+import { useState } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+import AnalyticsPage from './pages/AnalyticsPage';
 import './App.css';
-import LandingPage from './pages/LandingPage'
 
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={(props) => (
+          <LandingPage
+            {...props}
+            userLoggedIn={userLoggedIn}
+            setUserLoggedIn={setUserLoggedIn}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/dashboard"
+        render={(props) => (
+          <AnalyticsPage
+            {...props}
+            userLoggedIn={userLoggedIn}
+            setUserLoggedIn={setUserLoggedIn}
+          />
+        )}
+      />
+    </Switch>
+  </BrowserRouter>
   );
 }
 
