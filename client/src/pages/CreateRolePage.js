aaf5abb26c699b0ef008afaddfe93659963a99b8
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-18 13:46:29 -0500

    added create roles page

--- client/src/pages/CreateRolePage.js
+++ client/src/pages/CreateRolePage.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React from "react";
+import api from "../apis/api";
+import ErrorModal from "../components/ErrorModal";
+import RoleForm from "../components/RoleForm";
 
-const CreateRolePage = () => {
-  return <div>Create Role Page</div>
+class CreateRolePage extends React.Component {
+  state = { errorMessage: "" };
+
+  handleFormSubmit = (e, title, salary, departmentId) => {
+    e.preventDefault();
+    api
+      .post("/roles", {
+        title: title,
+        salary: salary,
+        departmentId: departmentId,
+      })
+      .then(
+        (response) => {
+          if (response.status === 200) {
+            this.props.history.push("/roles");
+          }
+        },
+        (error) => {
+          console.log(error);
+          this.setState({
+            errorMessage: "There was an error updating the role.",
+          });
+        }
+      );
+  };
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <h2 className="text-center mb-4">Create Role</h2>
+        {this.state.errorMessage ? (
+          <ErrorModal modalMessage={this.state.errorMessage} />
+        ) : (
+          <RoleForm handleFormSubmit={this.handleFormSubmit} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default CreateRolePage;
