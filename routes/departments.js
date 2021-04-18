6b861c700194ed67be3f6a0c61645c71444a4e0c
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-17 21:34:18 -0500

    added delete department route

--- routes/departments.js
+++ routes/departments.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 // Controllers
 const {
-  createDepartment, getDepartmentTableData, getSingleDepartmentInfo
+  createDepartment, deleteDepartment, getDepartmentTableData, getSingleDepartmentInfo
 } = require("../controllers/departments");
 
 router.route("/:departmentId").get(getSingleDepartmentInfo);
+router.route("/:departmentId").delete(deleteDepartment);
 router.route("/").post(createDepartment);
 router.route("/").get(getDepartmentTableData);
 
