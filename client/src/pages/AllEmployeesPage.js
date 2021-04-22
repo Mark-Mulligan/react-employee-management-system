41fcae93568e89b557498c0fda89f53358b3243f
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-21 20:42:59 -0500

    added employees page

--- client/src/pages/AllEmployeesPage.js
+++ client/src/pages/AllEmployeesPage.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import axios from "axios";
+import ErrorModal from "../components/modals/ErrorModal";
+
+import "./AllEmployeesPage.css";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -29,12 +34,58 @@ const columns = [
 ];
 
 const AllEmployeesPage = (props) => {
+  const [employees, setEmployees] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getEmployees = () => {
+    axios
+      .get("/api/employees")
+      .then((response) => {
+        setEmployees(response.data.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          setErrorMessage(error.response.statusText);
+        } else if (error.request) {
+          console.log(error.request);
+          setErrorMessage("There was an error connecting to the server.");
+        } else {
+          console.log("Error", error.message);
+          setErrorMessage("There was an error loading the page.");
+        }
+        console.log(error.config);
+      });
+  }; 
+
+  useEffect(() => {
+    if (props.userLoggedIn) {
+      getEmployees();
+    }
+  }, [props.userLoggedIn]);
+
   return (
-    <div>
-    {console.log(props)}
-    All Employees Page
+    <div className="mt-5">
+      <h2 className="text-center">Employees</h2>
+      {errorMessage ? (
+        <ErrorModal modalMessage={errorMessage} />
+      ) : (
+        <div className="employee-table-container">
+          <DataGrid
+            autoHeight={true}
+            rowHeight={30}
+            scrollbarSize={20}
+            rows={employees}
+            columns={columns}
+            disableSelectionOnClick={true}
+            onRowClick={(data) => {
+              props.history.push(`/employee/${data.row.id}`);
+            }}
+          />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default AllEmployeesPage;
